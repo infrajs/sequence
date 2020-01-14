9626e3cf4057931da58d14576ea6f4f78a228822
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-01-14 16:10:57 +0400

    fix

--- Seq.js
+++ Seq.js
@@ -1,12 +1,21 @@
 /*
  * autosave template session state
  * */
-Sequence = {
+export let Seq = {
 	seldom: '·',
 	offen: '.',
+	forr: function (el, callback) { //Бежим по массиву
+		if (!(el instanceof Array)) return;
+		let r,i,l;
+		for ( i = 0, l = el.length; i < l; i++ ) { //В callback нельзя удалять... так как i сместится
+			if (el[i] == null) continue;
+			r = callback(el[i], i, el); //callback,name,context,args,more
+			if (r != null) return r;
+		}
+	},
 	short: function (val,offen,seldom) {//Возвращает строку - короткая запись последовательности
-		offen=offen||infra.seq.offen;
-		seldom=seldom||infra.seq.seldom;
+		offen=offen||Seq.offen;
+		seldom=seldom||Seq.seldom;
 		if (typeof(val)=='string') return val;
 		if (!val || typeof(val) != 'object' || val.constructor != Array) val = [];
 		var nval=[];
@@ -18,17 +27,17 @@ Sequence = {
 		return nval.join(offen);
 	},
 	contain: function (search,subject) {
-		return !infra.forr(search, function (name,index) {
-			if (name!=subject[index]) return true;
+		return !Seq.forr(search, function (name, index) {
+			if (name != subject[index]) return true;
 		});
 	},
 	right: function (val,offen,seldom) {//Возвращает массив - правильную запись последовательности
-		offen=offen||infra.seq.offen;
-		seldom=seldom||infra.seq.seldom;
+		offen=offen||Seq.offen;
+		seldom=seldom||Seq.seldom;
 		if (!val||typeof(val)!=='object'||val.constructor!==Array) {
 			if (typeof(val)!='string')val='';
 			val=val.split(offen);
-			infra.forr(val, function (s,i) {
+			Seq.forr(val, function (s,i) {
 				val[i]=s.replace(seldom,offen);//Знак offen используется часто и должна быть возможность его указать в строке без специального смысла.. вот для этого и используется знак seldom 
 			});
 			if (val[val.length-1]==='')val.pop();
@@ -44,10 +53,6 @@ Sequence = {
 				res.push(s);
 			}
 		}
-		/* infra.forr(val, function (s) {//удаляются пустые
-			//if (s==='') return;
-			res.push(s);//Знак offen используется часто и должна быть возможность его указать в строке без специального смысла.. вот для этого и используется знак seldom 
-		});*/
 		return res;
 	},
 	add: function(obj, right, val)
@@ -55,33 +60,51 @@ Sequence = {
 		var i = right.length;
 		if (typeof(val) == 'undefined' || val === null) return obj;
 		if (!obj || typeof(obj) != 'object' || obj.constructor != Array) obj = [];
-		var need = Sequence.get(obj, right, 0, i, true);
+		var need = Seq._getmake(obj, right, 0, i);
 		if (~need.indexOf(val)) need.push(val);
 		return obj;
 	},
 	set: function (obj, right, val) {
-		var make=(typeof(val)=='undefined'||val===null?false:true);
-		var i=right.length-1;
+		var make = val == null ? false : true;
+		var i = right.length-1;
 		if (i==-1) return val;
 		if (make&&(!obj||typeof(obj)!=='object')&&typeof(obj)!=='function')obj={};
-		var need=infra.seq.get(obj,right,0,i,make);
+		if (!make) var need=Seq._get(obj,right,0,i);
+		else var need = Seq._getmake(obj,right,0,i);
 		if (!make&&(need&&typeof(need)=='object'))delete need[right[i]];
 		if (make)need[right[i]]=val;
 		return obj;
 	},
-	get: function (obj,right,start,end,make) {//получить из obj значение right до end брать начинаем с start
+	get: (obj, short, def = null) => {
+		let right = Seq.right(short);
+		let res = Seq._get(obj, right);
+		return (res == null) ? def : res;
+	},
+	_getmake: function (obj,right,start = 0,end = right.length) {//получить из obj значение right до end брать начинаем с start
 		if (typeof(start)==='undefined')start=0;
 		if (typeof(end)==='undefined')end=right.length;
-		if (end===start) return obj;
-		if (obj===undefined) return;
+		if (end === start) return obj;
+		if (obj == null) return;
 
-		if (make&&((!obj[right[start]]||typeof(obj[right[start]])!=='object')&&typeof(obj[right[start]])!=='function'))obj[right[start]]={};
+		if (((!obj[right[start]]||typeof(obj[right[start]])!=='object')&&typeof(obj[right[start]])!=='function')) obj[right[start]] = { };
 		if ((obj&&typeof(obj)=='object')||typeof(obj)=='function') {
 			if (((obj===location||(!obj.hasOwnProperty))&&obj[right[start]])||obj.hasOwnProperty(right[start])) {
 				//в ie у location есть свойство hasOwnProperty но все свойства не являются собственными у location. в ff у location нет метода hasOwnProperty
-				return infra.seq.get(obj[right[start]],right,++start,end,make);
+				return Seq._getmake(obj[right[start]],right,++start,end);
+			}
+		}
+	},
+	_get: function (obj,right,start = 0,end = right.length) {//получить из obj значение right до end брать начинаем с start
+		if (typeof(start)==='undefined')start=0;
+		if (typeof(end)==='undefined')end=right.length;
+		if (end === start) return obj;
+		if (obj == null) return;
+		if ((obj&&typeof(obj)=='object')||typeof(obj)=='function') {
+			if (((obj===location||(!obj.hasOwnProperty))&&obj[right[start]])||obj.hasOwnProperty(right[start])) {
+				//в ie у location есть свойство hasOwnProperty но все свойства не являются собственными у location. в ff у location нет метода hasOwnProperty
+				return Seq._get(obj[right[start]],right,++start,end);
 			}
 		}
 	}
 }
-if (window.infra) infra.seq = Sequence;
+export default Seq;
